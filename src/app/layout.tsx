795ca70880f92b8d34f2c121a3f7b1aec1a0c13c
Muhammed Easa <easanedumangad@gmail.com>
2024-05-29 12:41:55 +0530

    Added dark mode and changed the ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import useQuiz from "./store";
-
+import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -12,11 +12,25 @@ export default function RootLayout({
   children: React.ReactNode;
   quiz: React.ReactNode;
 }>) {
-  const config = useQuiz((state) => state.config);
+  const config = useQuiz((state:any) => state.config);
   let render = config.status === "start" ? quiz : children;
   return (
-    <html lang="en">
-      <body className={inter.className}>{render}</body>
+    <>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body  className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {render}
+        </ThemeProvider>
+      </body>
     </html>
+  </>
   );
 }
+
+
